91194cd9c63ae7e965dfb816a7e6d8468199ee28
Jey Lee <jeylee@jeylee-macbookpro2.roam.corp.google.com>
2019-06-24 13:58:08 -0700

    Add initial version for scraping

--- app/ai/views.py
+++ app/ai/views.py
@@ -15,13 +15,20 @@ def index(request):
 
 def result(request):
     query_text = request.POST.get('query_text')
-    response = "Search results for the query : " + query_text + " >> asin= : " + str(keyword_search(query_text))
+    asin_list = asin_scrape(query_text)
+
+    review_list = []
+    for x in range(5):
+        review_list = review_list + review_scrape(asin_list[0],x)
+
+    response = "Search results for the query : " + query_text + " <br> >> Product ASINs : " + str(asin_list)\
+     + "<br> >> Reviews = <br><br>" + str(review_list)
     return HttpResponse(response)
 
 
-def keyword_search(query_text):
+def asin_scrape(query_text):
     url_pre = 'https://www.amazon.com/s?k='
-    url_pro = '&ref=nb_sb_noss_2'
+    url_post = '&ref=nb_sb_noss_2'
 
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'}
     proxies_list = ["35.198.69.233:80","178.128.11.215:80","54.226.53.87:80"]
@@ -30,7 +37,7 @@ def keyword_search(query_text):
     parse_text_start = '=UTF8&amp;asin='
     parse_text_end = '&amp;ref'
 
-    url = url_pre + query_text + url_pro
+    url = url_pre + query_text + url_post
     print(url)
 
     response = requests.get(url, headers=headers, proxies=proxies).text
@@ -40,7 +47,6 @@ def keyword_search(query_text):
     parsed_asin_list = []
     if(response.find(parse_text_start) > 0):
         response_processing = response[response.find(parse_text_start)+len(parse_text_start):]
-        response_processing_link = response[response.find(parse_text_start)+len(parse_text_start):]
 
         cnt=0
         while cnt < 10:
@@ -56,3 +62,41 @@ def keyword_search(query_text):
         parsed_asin_list = ['NO_SEARCH_RESULT']
 
     return parsed_asin_list
+
+
+def review_scrape(product_asin, page_number):
+    url_pre = 'https://www.amazon.com/product-reviews/'
+    url_post = '/reviewerType=all_reviews&sortBy=recent&pageNumber='
+
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'}
+    proxies_list = ["35.198.69.233:80","178.128.11.215:80","54.226.53.87:80"]
+    proxies = {'http': random.choice(proxies_list)}
+
+    parse_text_start = 'review-text-content"><span class="">'
+    parse_text_end = '</span>'
+
+    url = url_pre + product_asin + url_post + str(page_number)
+    print(url)
+    response = requests.get(url, headers=headers, proxies=proxies).text
+
+    parsed_review_list = []
+    if(response.find(parse_text_start) > 0):
+        response_processing = response[response.find(parse_text_start)+len(parse_text_start):]
+
+        cnt=0
+        while cnt < 10:
+            parsed_review = response_processing[:response_processing.find(parse_text_end)]
+            parsed_review_list.append(parsed_review)
+            cnt += 1
+
+            if(response.find(parse_text_start) > 0):
+                response_processing = response_processing[response_processing.find(parse_text_start)+len(parse_text_start):]
+            else:
+                break
+    else:
+        parsed_review_list = ['NO_SEARCH_RESULT']
+
+    return parsed_review_list
+
+
+
